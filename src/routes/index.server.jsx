8ff3a50c1ccda905f9254a7f726812e6552b0aee
Loc Tran <mrlocspoc@gmail.com>
2022-11-20 15:33:10 +1030

    Tailwind tutorial completed

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -3,8 +3,25 @@ import Layout from "../components/Layout.server";
 export default function Home() {
 	return (
 		<Layout>
-			<div className="home-page container">
+			<div className="container pt-2 flex items-center justify-center flex-col prose">
 				<h1>Homepage</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Impedit beatae doloribus odit sapiente rem blanditiis
+					nostrum distinctio? Possimus veritatis ex placeat nulla fuga
+					vero, accusamus ducimus dignissimos, aut molestias ipsum.
+				</p>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+					Facere dolor reiciendis vero aliquid animi distinctio eos
+					nobis fugit quod, fugiat, architecto, sapiente tempore
+					ipsum? Necessitatibus inventore nostrum nesciunt sint aut.
+				</p>
+				<ul>
+					<li>One</li>
+					<li>Two</li>
+					<li>Three</li>
+				</ul>
 			</div>
 		</Layout>
 	);
